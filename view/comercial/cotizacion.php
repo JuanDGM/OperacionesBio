ea23e60bb7b28b7582878328f8158af8c8af7f9c
JuanDGM <juan84_10@hotmail.com>
2019-01-21 19:02:36 -0500

    first commit

---
+++ view/comercial/cotizacion.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>cotizacion</title>
+        <link rel="stylesheet" href="../../style/generalFrame.css">
+        <link rel="stylesheet" href="../../style/comercial/menuComercial.css">
+        <link rel="stylesheet" href="../../library/Bootstrap/css/bootstrap.min.css">
+        
+    </head>
+    <body>
+        
+        <div class="generalFrame">
+            <header class="header">
+                <div class="marcoencabezadoLateralIzq"><a href="../home.php">Regresar</a></div>
+                <div class="marcoCentral">COTIZACIÓN</div>
+                <div class="marcoencabezadoLateralDer"></div>
+            </header>
+            <nav class="nav">
+                    <button type="button" class="btn btn-success">COTIZACIÓN</button>
+                    <button type="button" class="btn btn-success">SEGUIMIENTO A COTIZACIONES</button>
+                    <button type="button" class="btn btn-success">CREAR CLIENTES</button>
+                    <button type="button" class="btn btn-success">PORTAFOLIO DE SERVICIOS</button>
+                    <button type="button" class="btn btn-success">CREAR OFERTAS</button>
+                    <button type="button" class="btn btn-success">OPERACIÓN COMERCIAL</button>
+            </nav>
+            
+            <article></article>
+            
+            <footer></footer>
+            
+            
+        </div>
+        
+    </body>
+</html>
